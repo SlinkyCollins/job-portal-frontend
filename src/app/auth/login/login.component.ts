92c930b90eaea23af238e70391336bc9208b0316
Ademola Afolabi <afolabiademola27@gmail.com>
2024-12-23 14:50:24 +0100

    feat: implement login functionality with password toggle and toastr notifications - Created login form with email and password inputs - Added password visibility toggle and clear functionality - Integrated backend login endpoint for authentication - Displayed toastr notifications based on login success or failure - Prepared for role-based dashboard redirection (to be implemented next)

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,12 +1,57 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, CommonModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  constructor
+  (
+    public router:Router,
+    public http:HttpClient,
+    public toastr: ToastrService
+  ){}
 
+  public password:string = '';
+  public email:string = '';
+  public showPassword:boolean = false;
+  public msg:string = '';
+
+clearPassword() {
+    this.password = '';
+  }
+
+  togglePassword() {
+    this.showPassword = !this.showPassword;
+  }
+
+  login() {
+    let userDetails = {
+      mail: this.email,
+      pword: this.password
+    }
+    this.http.post("http://localhost/JobPortal/login.php", userDetails).subscribe((data:any)=>{
+      console.log(data);
+      this.msg = data.msg;
+      if (data.status == true) {
+        this.toastr.success('Login successful');  
+        // this.router.navigate(['/']);
+      } else if (data.status == '401') {
+        this.toastr.error('Incorrect password');  
+      } else if (data.status = '404') {
+        this.toastr.error('User not found, please try signing up');  
+      }
+    }, (error:any)=>{
+      console.log(error);
+      console.log('Error Response:', error.error);
+    })
+  }
 }
--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -2,13 +2,13 @@ import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
@@ -20,13 +20,18 @@ export class SignupComponent {
     public toastr: ToastrService
   ){}
 
-  public firstname = '';
-  public lastname = '';
-  public email = '';
-  public password = '';
-  public role = '';
-  public showPassword = false;
-  public msg = '';
+  public firstname:string = '';
+  public lastname:string = '';
+  public email:string = '';
+  public password:string = '';
+  public role:string = '';
+  public showPassword:boolean = false;
+  public msg:string = '';
+
+  // ngOnInit() {
+  //   this.toastr.success('Toastr is working!');
+  // }
+
 
 
   signup(form: NgForm){
@@ -42,8 +47,7 @@ export class SignupComponent {
       this.msg = data.msg;
       if (data.status == '403') {
         this.toastr.info('Email already exists');  
-      }
-      if (data.status == true) {
+      } else if (data.status == true) {
         this.toastr.success('Signed up sucessfully');  
         this.router.navigate(['/login']);
       }
@@ -59,8 +63,4 @@ export class SignupComponent {
   togglePassword() {
     this.showPassword = !this.showPassword;
   }
-
-  // clearPassword() {
-  //   this.password = '';
-  // }
 }
