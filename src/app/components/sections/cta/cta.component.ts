a5fbb9e1d5542be541d1806b168177a23e57ddb6
Ademola Afolabi <afolabiademola27@gmail.com>
2025-10-20 11:18:52 +0100

    feat(cta): update user setup section to include login check and correct routing for job posting

--- src/app/components/sections/cta/cta.component.ts
+++ src/app/components/sections/cta/cta.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { AuthService } from '../../../core/services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cta',
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './cta.component.html',
   styleUrl: './cta.component.css'
 })
 export class CtaComponent {
+ constructor (private authService: AuthService) {}
 
+ isLoggedIn(): boolean {
+  return this.authService.isLoggedIn();
+ }
 }
