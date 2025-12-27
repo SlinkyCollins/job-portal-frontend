2f57f62f3f745752d778d8450cc74a30fb187440
Ademola Afolabi <afolabiademola27@gmail.com>
2025-12-27 08:19:41 +0100

    feat(ui): update links to use routerLink for improved navigation and accessibility

--- src/app/shared/sections/category-list/category-list.component.ts
+++ src/app/shared/sections/category-list/category-list.component.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-category-list',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './category-list.component.html',
   styleUrl: './category-list.component.css'
 })
